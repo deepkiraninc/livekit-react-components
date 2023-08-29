9489262f8d3a298ac91cb76e0ed3dd072906ded1
root <root@ip-172-31-23-63.us-west-2.compute.internal>
2023-08-29 05:20:47 +0000

    Yarn built update from jenkins for components

--- src/prefabs/ControlBar.tsx
+++ src/prefabs/ControlBar.tsx
@@ -208,8 +208,8 @@ export function ControlBar({
       {visibleControls.endForAll ? (
         <div className='tl-leave'>
           <button className="lk-disconnect-button">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none"><path fill="currentColor" fill-rule="evenodd" d="M2 2.75A2.75 2.75 0 0 1 4.75 0h6.5A2.75 2.75 0 0 1 14 2.75v10.5A2.75 2.75 0 0 1 11.25 16h-6.5A2.75 2.75 0 0 1 2 13.25v-.5a.75.75 0 0 1 1.5 0v.5c0 .69.56 1.25 1.25 1.25h6.5c.69 0 1.25-.56 1.25-1.25V2.75c0-.69-.56-1.25-1.25-1.25h-6.5c-.69 0-1.25.56-1.25 1.25v.5a.75.75 0 0 1-1.5 0v-.5Z" clip-rule="evenodd"></path><path fill="currentColor" fill-rule="evenodd" d="M8.78 7.47a.75.75 0 0 1 0 1.06l-2.25 2.25a.75.75 0 1 1-1.06-1.06l.97-.97H1.75a.75.75 0 0 1 0-1.5h4.69l-.97-.97a.75.75 0 0 1 1.06-1.06l2.25 2.25Z" clip-rule="evenodd"></path></svg>
-            {visibleControls.leaveButton}
+            {showIcon && <LeaveIcon />}
+            {showText && visibleControls.leaveButton}
           </button>
 
           <div className='tl-leave-btn'>
